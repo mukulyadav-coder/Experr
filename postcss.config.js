db80dab11348626baf9b5255c1f1f7353d4228e7
mukulyadav <2006mukulyadav@gmail.com>
2026-04-02 22:56:57 +0530

    UI Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
 export default {
+    //pluggins
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //pluggins
   plugins: [react()],
   server: {
     allowedHosts: ["provision-improving-dont-distance.trycloudflare.com"],
